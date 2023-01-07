6fb663cbdcdbdc7e8dcccd1e7a8009ffab306389
Manoj Kumar Sahoo <112025244+manojkumarmks@users.noreply.github.com>
2023-01-07 21:36:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  const emp=arr.map(p => {if(p.profession=="developer"){
+   
+      console.log(p.profession);
+  }})
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.foreach(a => {
+    if(a.profession=="developer")
+    {
+      console.log(p.profession);
+    }
+  })
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+  console.log(arr);
+//   arr=arr.concat(arr2)
+  
+
 }
 
 function removeAdmin() {
   //Write your code here
-}
+  const emp=arr.map(p => {if(p.profession=="admin"){
+   
+       delete p.profession;
+    console.log(arr);
+}})}
 
 function concatenateArray() {
   //Write your code here
+  let arr2=[{id:5,name:"mathew",age:"25",profession:"tester"}];
+  arr=arr.concat(arr2);
+  console.log(arr);
+  
 }
+//calling the functions
+PrintDeveloperswithMap();
+PrintDeveloperbyForEach();
+addData();
+removeAdmin();
+concatenateArray();
